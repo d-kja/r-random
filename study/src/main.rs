4046da901c060cbf22f45a7fc00695e9bd9f9bd4
d-kja <89911360+d-kja@users.noreply.github.com>
2024-01-31 01:59:03 -0300

    fix

--- study/src/main.rs
+++ study/src/main.rs
@@ -1,151 +1,34 @@
-#[derive(Debug)]
-enum Status {
-    AUTHORIZED,
-    UNAUTHORIZED,
-}
-
-#[derive(Clone, Debug)]
-enum AccessLevel {
-    HIGH,    // 1000
-    _MEDIUM, // 800
-    LOW,     // 500
-}
-
-impl AccessLevel {
-    fn get_level(&self) -> u16 {
-        match self {
-            AccessLevel::HIGH => 1000,
-            AccessLevel::_MEDIUM => 800,
-            AccessLevel::LOW => 500,
-        }
-    }
-}
-
-#[derive(Clone, Debug)]
-struct Employee {
+#[derive(Debug, Clone)]
+struct Person {
     name: String,
-    keycard: Option<AccessLevel>,
+    age: u8,
 }
 
-impl Employee {
-    fn get_keycard(&self) -> Result<&AccessLevel, &str> {
-        match &self.keycard {
-            Some(value) => Ok(value),
-            None => Err("doesn't have a keycard"),
-        }
-    }
-}
-
-struct Database {
-    employees: Vec<Employee>,
-}
-
-impl Database {
-    fn connect() -> Self {
-        Self {
-            employees: Vec::new(),
-        }
-    }
-
-    fn update_employees(&mut self, employees: Vec<Employee>) {
-        self.employees = employees;
-    }
-
-    fn _filter_employee(&self, name: &str) {
-        let employee = self
-            .employees
-            .iter()
-            .filter(|&value| {
-                if value.name.eq(name) {
-                    return true;
-                }
-
-                false
-            })
-            .cloned()
-            .collect::<Vec<Employee>>();
-
-        println!("{employee:?}");
-    }
-
-    fn find_employee(&self, name: &str) -> Result<&Employee, &str> {
-        let employee = self.employees.iter().find(|&value| value.name.eq(name));
-
-        match employee {
-            Some(value) => Ok(value),
-            None => Err("employee not found"),
-        }
-    }
-}
-
-struct Location {
-    required_access_level: u16,
-}
-
-impl Location {
-    fn has_access(self: &Self, keycard: &AccessLevel) -> Result<Status, String> {
-        let level = keycard.get_level();
-
-        if level < self.required_access_level {
-            return Err(format!(
-                "doesn't meet the requirements, level required {} is higher than {}",
-                self.required_access_level, level
-            ));
-        }
-
-        Ok(Status::AUTHORIZED)
-    }
-}
-
-fn main() -> Result<(), String> {
-    let result = authorize();
-
-    if let Err((err, status)) = result {
-        println!("Access not granted! {} | Status: {:?}", err, status);
-    }
+fn main() -> Result<(), &'static str> {
+    find_by_name("jose");
 
     Ok(())
 }
 
-fn authorize() -> Result<Status, (String, Status)> {
-    let mut db = Database::connect();
-
-    // Private location, requires high access level
-    let location = Location {
-        required_access_level: 900,
-    };
-
-    // Creating mock data
-    db.update_employees(vec![
-        Employee {
-            name: "john".to_owned(),
-            keycard: None,
+fn find_by_name(name: &str) -> Result<Person, &'static str> {
+    let people = vec![
+        Person {
+            name: String::from("john"),
+            age: 18,
         },
-        Employee {
-            name: "jose".to_owned(),
-            keycard: Some(AccessLevel::LOW),
+        Person {
+            name: String::from("jose"),
+            age: 20,
         },
-    ]);
+    ];
+    let found_person = match people
+        .iter()
+        .find(|&person| person.name.eq(name)) {
+             Some(value) => value.clone(),
+             None => return Err("not found.")
+        }; 
 
-    let res = db.find_employee("john");
+    dbg!(&found_person);
 
-    if let Ok(employee) = res {
-        let employee_keycard = match employee.get_keycard() {
-            Ok(value) => value,
-            Err(err) => panic!("{err}"),
-        };
-
-        let has_access: Result<Status, String> = location.has_access(&employee_keycard);
-
-        return match has_access {
-            Ok(_) => {
-                println!("granted");
-
-                Ok(Status::AUTHORIZED)
-            }
-            Err(err) => Err((err, Status::UNAUTHORIZED)),
-        };
-    }
-
-    Err(("employee not found".to_owned(), Status::UNAUTHORIZED))
+    Ok(found_person)
 }
--- study/study-compilation/exercises/exercise_19.rs
+++ study/study-compilation/exercises/exercise_19.rs
@@ -1,27 +1,34 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Person {
     name: String,
-    age: u8
+    age: u8,
 }
 
 fn main() -> Result<(), &'static str> {
-   find_by_name("jose");
+    find_by_name("jose");
 
-   Ok(())
+    Ok(())
 }
 
 fn find_by_name(name: &str) -> Result<Person, &'static str> {
     let people = vec![
         Person {
-            name: String::new("john"),
-            age: 18
+            name: String::from("john"),
+            age: 18,
         },
         Person {
-            name: String::new("jose"),
-            age: 20
-        }          
+            name: String::from("jose"),
+            age: 20,
+        },
     ];
-    let found_person = people.iter().find(|person| { person.name.eq(name) });
+    let found_person = match people
+        .iter()
+        .find(|&person| person.name.eq(name)) {
+             Some(value) => value.clone(),
+             None => return Err("not found.")
+        }; 
 
-    dbg!(found_person);
+    dbg!(&found_person);
+
+    Ok(found_person)
 }
